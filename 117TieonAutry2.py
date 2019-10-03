d592c7b140e6f7764e1e119dbbd41ad40c679975
Tieonautry12 <55498243+Tieonautry12@users.noreply.github.com>
2019-10-03 10:18:05 -0400

    Add files via upload

--- 117TieonAutry2.py
+++ 117TieonAutry2.py
@@ -1,23 +1,26 @@
-print() # blank line
-print("Enter your points for " + course)
+my_courses = ["English", "Math", "CS"]
 
-a = int(input("Points -> "))
+check_grades = True
+while (check_grades):
 
-for course in my_courses
-
-    redo = input("Do you need to re-do these grades? (y/n)")
-if (redo == "y"):
-	check_grades = True
-else:
-	check_grades = False
-
-if (a >= 90):
-	print("A")
-elif (a >= 80):
-	print("B")
-elif (a >= 70):
-	print("C")
-elif (a >= 60):
-	print("D")
-else:
-	print("F")
+	for course in my_courses:
+		print() # blank line
+		print("Enter your points for " + course)	
+		
+		a = int(input("Points -> "))
+		if (a >= 90):
+			print("A")
+		elif (a >= 80):
+			print("B")
+		elif (a >= 70):
+			print("C")
+		elif (a >= 60):
+			print("D")
+		else:
+			print("F")
+	
+	redo = input("Do you need to re-do these grades? (y/n)")
+	if (redo == "y"):
+		check_grades = True
+	else:
+		check_grades = False
